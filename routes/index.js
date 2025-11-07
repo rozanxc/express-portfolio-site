efe603efe5bf67e803f0f79070c74b964e82290a
rozanxc <rghum7807@gmail.com>
2025-11-07 14:11:50 -0500

    1st commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+// Home
+router.get('/', function(req, res) {
+  res.render('pages/index', { title: 'Home' });
+});
+
+// About
+router.get('/about', function(req, res) {
+  res.render('pages/about', { title: 'About Me' });
+});
+
+// Projects
+router.get('/projects', function(req, res) {
+  res.render('pages/projects', { title: 'Projects' });
+});
+
+// Contact
+router.get('/contact', function(req, res) {
+  res.render('pages/contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
